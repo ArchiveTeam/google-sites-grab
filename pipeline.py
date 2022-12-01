631cdd82b3f73470d6f0b449c2ea60cf248645fd
arkiver <arkiver@protonmail.com>
2022-12-01 17:46:53 +0100

    Version 20221201.01. Support GNU Wget 1.21.3-at.20220608.02.

--- pipeline.py
+++ pipeline.py
@@ -39,7 +39,10 @@ if StrictVersion(seesaw.__version__) < StrictVersion('0.8.5'):
 
 WGET_AT = find_executable(
     'Wget+AT',
-    ['GNU Wget 1.20.3-at.20211001.01'],
+    [
+        'GNU Wget 1.20.3-at.20211001.01',
+        'GNU Wget 1.21.3-at.20220608.02'
+    ],
     [
         './wget-at',
         '/home/warrior/data/wget-at'
@@ -55,7 +58,7 @@ if not WGET_AT:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20211001.01'
+VERSION = '20221201.01'
 USER_AGENT = 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'
 TRACKER_ID = 'google-sites'
 TRACKER_HOST = 'legacy-api.arpa.li'
